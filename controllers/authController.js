77daa8dfb7743fd246bd13993f813c354bbb3682
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-06-15 23:26:25 +0000

    Create login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,3 +15,37 @@ const registerUser = async (req, res) => {
 
   res.status(201).json({ msg: "User registered successfully" });
 };
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find((u) => u.username === username);
+
+  if (!user) {
+    return res.status(400).json({ msg: "Invalid credential" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ msg: "Invalid credential" });
+  }
+
+  const accessToken = jwt.sign({ username: user.username }, ACCESS_KEY, {
+    expiresIn: "15m",
+  });
+
+  const refreshToken = jwt.sign({ username: user.username }, REFRESH_KEY, {
+    expiresIn: "7d",
+  });
+
+  refreshTokens.push(refreshToken);
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: environment === "production" ? true : false,
+    sameSite: environment === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.json({ accessToken, msg: "Login successful" });
+};
