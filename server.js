2b9fa5fc84d0b8f2821cc0a3c5e0ccf7ef2e5b57
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-06-15 23:40:21 +0000

    Import cookieParser in server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth");
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
