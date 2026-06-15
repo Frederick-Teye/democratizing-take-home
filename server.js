4816715a815c77baaceba56883a26f728984733c
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-06-15 08:09:25 +0000

    Update port assigned value

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
-
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
