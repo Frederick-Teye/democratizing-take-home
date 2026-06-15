a0fe5598f3b6f7e6d953b849c7b74bf4f37a6b70
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-06-15 23:49:01 +0000

    Create authenticateJWTMiddleware

---
+++ middleware/authenticateJWTMiddleware.js
@@ -0,0 +1,27 @@
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
+
+  // Extract token from "Bearer <token>"
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, ACCESS_KEY);
+
+    req.user = decoded.username;
+
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
+
+
+module.exports = authenticateJWT
