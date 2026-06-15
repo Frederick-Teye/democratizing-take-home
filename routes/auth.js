87e0727a621c12a97e5d047be49891dc38b54a64
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-06-15 23:49:42 +0000

    Further clearn auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,8 @@ const {
   logout,
 } = require("../controllers/authController");
 
+const authenticateJWT = require("../middleware/authenticateJWTMiddleware");
+
 const ACCESS_KEY = process.env.ACCESS_KEY || "accessKey";
 const REFRESH_KEY = process.env.REFRESH_KEY || "refreshKey";
 const environment = process.env.environment || "production";
@@ -16,33 +18,6 @@ const environment = process.env.environment || "production";
 router.post("/register", registerUser);
 
 router.post("/login", login);
-
-const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res.status(401).json({ message: "Authorization header missing" });
-  }
-
-  // Extract token from "Bearer <token>"
-  const token = authHeader.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ message: "Token missing" });
-  }
-
-  try {
-    // Verify token
-    const decoded = jwt.verify(token, ACCESS_KEY);
-
-    // Attach user to request
-    req.user = decoded.username;
-
-    next();
-  } catch (error) {
-    return res.status(403).json({ message: "Invalid or expired token" });
-  }
-};
 
 router.get("/profile", authenticateJWT, getProfile);
 
