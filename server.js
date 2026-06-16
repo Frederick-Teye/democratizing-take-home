a88d7b7acc9801f49eea0f738659c71541a7232d
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-06-16 00:08:41 +0000

    Import and use logger

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth");
+const logger = require("./middleware/logger");
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(logger);
 
 app.use("/api/auth", authRoutes);
 
